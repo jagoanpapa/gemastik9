35bf18985b915baffb249f5a056801eb91fda697
Bertonare <bertonarie123@gmail.com>
2016-07-04 10:20:40 +0700

    Add files via upload

---
+++ edited/about.php
@@ -0,0 +1,75 @@
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>MAHASISWA PUNYA ACARA</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<script type="text/javascript" src="js/jquery.js"></script>
+	<script type="text/javascript" src="js/bootstrap.js"></script>
+</head>
+<body>
+	<nav class="navbar navbar-default">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<a class="navbar-brand" href="index.php">IKI LOGO</a>
+			</div>
+			<ul class="nav navbar-nav">
+				<li ><a href="index.php">Home</a></li>
+				<li><a href="lihatacara.php">Lihat Acara</a></li>
+				<li><a href="uploadacara.php">Upload Acara</a></li> 
+				<li class="active"><a href="about.php">About</a></li> 
+			</ul>
+			<ul class="nav navbar-nav navbar-right">
+			<li><a href="daftar.php"><span class="glyphicon glyphicon-user"></span> Daftar</a></li>
+				<li><a href="login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+			</ul>
+		</div>
+	</nav>
+	
+	
+	<div class="container" style="margin-top:5%">
+    <div class="row">
+        <div class='col-lg-8'>
+			<div class="container">
+				<div class='col-lg-3'>
+				<img src="img/cek2.jpg" alt="poster fail loaded" style="width:264px;height:328px;">
+				</div>
+				<div class='col-lg-5'>
+				<a style="font-size: 200%;" >SKM WORKSHOP & COMPETITION</a><br><br>
+				<h4><span class="glyphicon glyphicon-tags"></span>&nbsp;&nbsp;Kategori</h4><br>
+				<h4><h4><span class="glyphicon glyphicon-user"></span>&nbsp;&nbsp;Penyelengara</h4><br>
+				<h4><h4><span class="glyphicon glyphicon-calendar"></span>&nbsp;&nbsp;Tanggal</h4><br>
+				<h4><h4><span class="glyphicon glyphicon-map-marker"></span>&nbsp;&nbsp;Lokasi</h4><br>
+				</div>
+			
+				<br> <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><div class='col-lg-8'>
+				<h4><span class="glyphicon glyphicon-info-sign"></span>&nbsp;&nbsp;Informasi Lengkap</h4>
+				</div>
+			
+		</div>
+		</div>
+		<div class='col-lg-4'>
+		
+           <br><h3>Acara Ini Masih Butuh Sponsor!</h3><br>
+		   &nbsp;&nbsp;&nbsp;&nbsp;<button style="width:130px;height:50;"><a>Sponsorship</a></button>
+        </div> 
+        <script type="text/javascript">
+            $(function () {
+                $('#datetimepicker1').datetimepicker();
+            });
+        </script>
+    </div>
+</div>
+ 	<div class="wrapper">
+	    <div class="container">
+		    <header class="hero-unit">
+		    </header>
+	    </div>
+	    <div class="push"><!--//--></div>
+	 </div>
+	    <footer>
+	<div class="container">
+    	<p>IKI FOOTER</p>
+	</div>
+</body>
+</html>
---
+++ edited/lihatacara.php
@@ -0,0 +1,96 @@
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>MAHASISWA PUNYA ACARA</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<script type="text/javascript" src="js/jquery.js"></script>
+	<script type="text/javascript" src="js/bootstrap.js"></script>
+</head>
+<body>
+	<nav class="navbar navbar-default">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<a class="navbar-brand" href="index.php">IKI LOGO</a>
+			</div>
+			<ul class="nav navbar-nav">
+				<li ><a href="index.php">Home</a></li>
+				<li class="active"><a href="lihatacara.php">Lihat Acara</a></li>
+				<li><a href="uploadacara.php">Upload Acara</a></li> 
+				<li><a href="about.php">About</a></li> 
+			</ul>
+			<ul class="nav navbar-nav navbar-right">
+			<li><a href="daftar.php"><span class="glyphicon glyphicon-user"></span> Daftar</a></li>
+				<li><a href="login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+			</ul>
+		</div>
+	</nav>
+	
+	<h1>LIST ACARA </h1> 
+	
+	<div class='col-lg-8'>
+	<div class="navbar2">
+		<div class="row">
+			<div class="col-xs-12">
+		        <div class="col-xs-4"> 
+					<div class='input-group date' id='search'>
+						<input type='text' class="form-control" placeholder="Cari Acara">
+						<span class="input-group-addon">
+							<span class="glyphicon glyphicon-search"></span>
+						</span>
+					</div>
+						
+				</div>
+		        
+		        <div class="form-group">
+			        <div class="col-xs-4 selectContainer">
+			            <select class="form-control" name="size">
+			                <option value="">Pilih Kategori</option>
+			                <option value="1">Workshop</option>
+		                    <option value="2">Seminar</option>
+		                    <option value="3">Lomba</option>
+		                    <option value="4">Seni</option>
+		                    <option value="5">Festival</option>
+		                    <option value="6">Kary Tulis</option>
+		                    <option value="7">Lain-Lain</option>
+			            </select>
+			        </div>
+			    </div>
+
+			    <div class="form-group">
+		        	<div class="col-xs-2" id="terpopuler"><button class="form-control" style="margin-top:-12.5%;"> Terpopuler</button> </div>
+		        </div>	
+		        <div class="form-group">
+		        	<div class="col-xs-2" id="terbaru"><button class="form-control" style="margin-top:-12.5%;"> Terbaru</button> </div>
+		        </div>	
+		    </div>
+	    </div>
+	</div>
+	<table  style="border-collapse:separate;border-spacing:3em;" >
+	<tr>
+		<td class="jco" align="center"  style="vertical-align:middle;width:270px; height:350px;"><img  src="img/cek2.jpg" alt="poster fail loaded" style="width:200px;height:300px;padding-bottom:7%"><br>ederw3de</td>
+		<td class="jco" align="center"  style="vertical-align:middle;width:270px; height:350px;"><img  src="img/cek2.jpg" alt="poster fail loaded" style="width:200px;height:300px;padding-bottom:7%"><br>ederw3de</td>
+		<td class="jco" align="center"  style="vertical-align:middle;width:270px; height:350px;"><img  src="img/cek2.jpg" alt="poster fail loaded" style="width:200px;height:300px;padding-bottom:7%"><br>ederw3de</td>
+	</tr style="padding-bottom: 1em;">
+	<tr><br>
+		<td class="jco" align="center"  style="vertical-align:middle;width:270px; height:350px;"><img  src="img/cek2.jpg" alt="poster fail loaded" style="width:200px;height:300px;padding-bottom:7%"><br>ederw3de</td>
+		<td class="jco" align="center"  style="vertical-align:middle;width:270px; height:350px;"><img  src="img/cek2.jpg" alt="poster fail loaded" style="width:200px;height:300px;padding-bottom:7%"><br>ederw3de</td>
+		<td class="jco" align="center"  style="vertical-align:middle;width:270px; height:350px;"><img  src="img/cek2.jpg" alt="poster fail loaded" style="width:200px;height:300px;padding-bottom:7%"><br>ederw3de</td>
+		
+	</tr>
+	</table>
+	</div>
+ 	<div class="wrapper">
+	    <div class="container">
+		    <header class="hero-unit">
+	
+		    </header>
+	    </div>
+	    <div class="push"><!--//--></div>
+	 </div>
+	    <footer>
+	<div class="container">
+    	<p>IKI FOOTER</p>
+	</div>
+</body>
+</html>
